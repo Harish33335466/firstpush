a043827991addeddf4211686695456d14044ccfc
Harishwaran <harishwaran.mail@gmail.com>
2024-09-15 19:55:51 +0530

    I am adding some comment in helloworld js

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,9 @@
 console.log("I am displayig something for u ");
 
+function mainfn(){
+    console.log("I am adding main function in main branch")
+}
+
+mainfn()
+
 var b="I am adding the second line";
